811b6d4d8295b0da55896bb553423e1c80fa8854
trailjeep <trailjeep@gmail.com>
2024-05-10 16:52:15 +0000

    Update init.sh

--- systemd_enable/init.sh
+++ systemd_enable/init.sh
@@ -43,10 +43,10 @@ fi
 if gum confirm "Do you want to switch to zen kernel?"; then
   echo "Installing zen kernel..."
   sudo pacman -S --needed --noconfirm linux-zen linux-zen-headers
-  sudo sed -i 's/GRUB_DEFAULT=.*$/GRUB_DEFAULT=saved/' /etc/default/grub
-  sudo sed -i '/GRUB_DEFAULT=.*$/aGRUB_SAVEDEFAULT=true' /etc/default/grub
-  sudo sed -i 's/GRUB_TIMEOUT=.*$/GRUB_TIMEOUT=2/' /etc/default/grub
-  sudo grub-mkconfig -o /boot/grub/grub.cfg
+  #sudo sed -i 's/GRUB_DEFAULT=.*$/GRUB_DEFAULT=saved/' /etc/default/grub
+  #sudo sed -i '/GRUB_DEFAULT=.*$/aGRUB_SAVEDEFAULT=true' /etc/default/grub
+  #sudo sed -i 's/GRUB_TIMEOUT=.*$/GRUB_TIMEOUT=2/' /etc/default/grub
+  #sudo grub-mkconfig -o /boot/grub/grub.cfg
 else
   echo "Not installing zen kernel"
 fi
