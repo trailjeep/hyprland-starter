bcbc46d63408ead46b2e0194e4547be47875315c
trailjeep <trailjeep@gmail.com>
2024-05-11 19:07:29 +0000

    Update init.sh

--- general_packages/init.sh
+++ general_packages/init.sh
@@ -2,5 +2,3 @@ echo "General Packages"
 echo "Installing general packages thats needed for any install."
 _installPackagesPacman "general_packages/packages.txt"
 echo "All packages installed correctly."
-echo "Createing common used directories"
-xdg-user-dirs-update
